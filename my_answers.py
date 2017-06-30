559ebe4d1f4d0deb6165fd07eddf518795c4d39e
helenwang11 <ywang010@gmail.com>
2017-06-30 10:32:52 -0600

    submit homework

--- my_answers.py
+++ my_answers.py
@@ -9,9 +9,14 @@ import keras
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series,window_size):
+    
     # containers for input/output pairs
     X = []
     y = []
+    
+    for i in range(len(series) - window_size):
+        X.append(series[i:i + window_size])
+        y.append(series[i + window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -23,15 +28,33 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5,input_shape = (window_size,step_size)))
+    model.add(Dense(1, activation='tanh'))
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
-
+    set(text)
 
     # remove as many non-english characters and character sequences as you can 
+    text = text.replace('à','a')
+    text = text.replace('â','a')
+    text = text.replace('è','e')
+    text = text.replace('é','e')
+    text = text.replace('&',' ')
+    text = text.replace('*',' ')
+    text = text.replace('-',' ')
+    text = text.replace('/',' ')
+    text = text.replace('@',' ')
+    text = text.replace('$','')
+    text = text.replace('%','')
+    text = text.replace("'",'')
+    text = text.replace('"','')
+    text = text.replace('(','')
+    text = text.replace(')','')
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -40,6 +63,8 @@ def window_transform_text(text,window_size,step_size):
     inputs = []
     outputs = []
     
-
-    
+    for i in range(0,len(text) - window_size,step_size):
+        inputs.append(text[i:i + window_size])
+        outputs.append(text[i + window_size])
+        
     return inputs,outputs
